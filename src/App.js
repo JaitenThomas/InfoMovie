cb7a50481071c2336c8c5b79fdb4f2f0fa78e04e
Jaiten Thomas <jaiten.thomas@gmail.com>
2019-07-13 16:00:01 +1000

    added react navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator
+} from 'react-navigation';
+
+import MovieScreen from './screens/MovieScreen';
+import TelevisionScreen from './screens/TelevisionScreen';
+
 class App extends Component {
   state = {};
   render() {
     return (
       <View>
-        <Text>Welcome from App.js</Text>
+        <Text>Hello World</Text>
       </View>
     );
   }
 }
 
-export default App;
+const TabNavigator = createBottomTabNavigator({
+  Movie: MovieScreen,
+  Television: TelevisionScreen
+});
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: TabNavigator
+  }
+});
+
+export default createAppContainer(AppNavigator);
---
+++ src/screens/index.js
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+
+import { createStackNavigator, createAppContainer } from 'react-navigation';
