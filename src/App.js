1847e4e431bc654c9b34a936acb0e6a55eb0436b
Jaiten Thomas <jaiten.thomas@gmail.com>
2019-07-13 15:04:05 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <View>
+        <Text>Welcome from App.js</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
